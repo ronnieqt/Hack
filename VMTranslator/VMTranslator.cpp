ceedc550973886415eb5314157362a6f4593aadc
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-06-04 17:38:13 +0800

    init for VM translator

---
+++ VMTranslator/VMTranslator.cpp
@@ -0,0 +1,15 @@
+/*
+* Program: The Hack Translator (vm code to assembly code)
+* Author : Xi (Ronnie) Chen
+* Date   : 06/04/2022
+* Usage  : prompt> VMTranslator ProgName.vm
+*/
+
+// ============================================================ //
+
+int main()
+{
+	return 0;
+}
+
+// ============================================================ //
