49df343c394885d3900a40ebd3b98f54c86de382
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-07-02 23:06:23 +0800

    moved testing code to test.py

--- Compiler/JackAnalyzer.py
+++ Compiler/JackAnalyzer.py
@@ -7,12 +7,6 @@ The main program that sets up and invokes the other modules.
 from pathlib import Path
 
 import argparse
-import xml.etree.ElementTree as ET
-
-from TokenTypes import TokenType
-from JackTokenizer import JackTokenizer
-
-from utils import pretty_print
 
 # %% command-line arguments
 
@@ -20,40 +14,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("src", help="Jack source file(s)")
 options = parser.parse_args()
 
-# %% process one jack file
+# %% process each jack file
 
-def process_jack_file(jack_file_path: Path):
-
-    tknzr = JackTokenizer(jack_file_path)
-
-    tokens = ET.Element("tokens")
-
-    while tknzr.has_more_tokens():
-
-        tknzr.advance()
-
-        if tknzr.token_type() == TokenType.KEYWORD:
-            elem = ET.SubElement(tokens, "keyword")
-            elem.text = tknzr.keyword()
-        elif tknzr.token_type() == TokenType.SYMBOL:
-            elem = ET.SubElement(tokens, "symbol")
-            elem.text = tknzr.symbol()
-        elif tknzr.token_type() == TokenType.INT_CONST:
-            elem = ET.SubElement(tokens, "integerConstant")
-            elem.text = str(tknzr.int_val())
-        elif tknzr.token_type() == TokenType.STRING_CONST:
-            elem = ET.SubElement(tokens, "stringConstant")
-            elem.text = tknzr.string_val()
-        elif tknzr.token_type() == TokenType.IDENTIFIER:
-            elem = ET.SubElement(tokens, "identifier")
-            elem.text = tknzr.identifier()
-        else:
-            raise Exception(f"Unrecognized Token Type: [{tknzr.token_type()}]")
-    
-    pretty_print(tokens)
-
-    xml_file_path = jack_file_path.with_name(f"{jack_file_path.stem}T.xml")
-    ET.ElementTree(tokens).write(xml_file_path)
+def process_jack_file(jack_file: Path):
+    pass
 
 # %% main function
 
@@ -61,14 +25,11 @@ def _main():
 
     # cmd-line arg parsing
     src = Path(options.src)
-    if not src.exists():
-        print(f"[{src}] does not exist!")
-        return
-    elif src.is_dir():
-        for jack_file in src.glob("*.jack"):
-            process_jack_file(jack_file)
-    else:  # is_file
-        process_jack_file(src)
+    jack_files = [*src.glob("*.jack")] if src.is_dir() else [src]
+
+    # process each jack file
+    for jack_file in jack_files:
+        process_jack_file(jack_file)
 
 # %% call the main function
 
---
+++ Compiler/test.py
@@ -0,0 +1,63 @@
+# %% import libs
+
+from pathlib import Path
+
+import argparse
+import xml.etree.ElementTree as ET
+
+from utils import pretty_print
+from TokenTypes import TokenType
+from JackTokenizer import JackTokenizer
+
+# %% test implementation of the tokenizer
+
+def test_tokenizer(jack_file_path: Path):
+
+    tknzr = JackTokenizer(jack_file_path)
+
+    tokens = ET.Element("tokens")
+
+    while tknzr.has_more_tokens():
+
+        tknzr.advance()
+
+        if tknzr.token_type() == TokenType.KEYWORD:
+            elem = ET.SubElement(tokens, "keyword")
+            elem.text = tknzr.keyword()
+        elif tknzr.token_type() == TokenType.SYMBOL:
+            elem = ET.SubElement(tokens, "symbol")
+            elem.text = tknzr.symbol()
+        elif tknzr.token_type() == TokenType.INT_CONST:
+            elem = ET.SubElement(tokens, "integerConstant")
+            elem.text = str(tknzr.int_val())
+        elif tknzr.token_type() == TokenType.STRING_CONST:
+            elem = ET.SubElement(tokens, "stringConstant")
+            elem.text = tknzr.string_val()
+        elif tknzr.token_type() == TokenType.IDENTIFIER:
+            elem = ET.SubElement(tokens, "identifier")
+            elem.text = tknzr.identifier()
+        else:
+            raise Exception(f"Unrecognized Token Type: [{tknzr.token_type()}]")
+    
+    pretty_print(tokens)
+
+    xml_file_path = jack_file_path.with_name(f"{jack_file_path.stem}T.xml")
+    ET.ElementTree(tokens).write(xml_file_path)
+
+# %% main function for testing
+
+def _main(src: Path):
+
+    jack_files = [*src.glob("*.jack")] if src.is_dir() else [src]
+    for jack_file in jack_files:
+        test_tokenizer(jack_file)
+
+# %% calling the main function
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("src", help="Jack source file(s)")
+    options = parser.parse_args()
+
+    _main(Path(options.src))
