3293ab5776e668f93f65d87ebfa61956bd1d6aa7
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-07-17 10:17:13 +0800

    implemented the vm writer class

--- Compiler/VMWriter.py
+++ Compiler/VMWriter.py
@@ -16,46 +16,46 @@ class VMWriter:
     def __init__(self, vm_file_path: Path) -> None:
         """Creates a new output .vm file/stream, and prepares it for writing."""
         self.f_vm = open(vm_file_path, "w")
-        pass
 
     def write_push(self, segment: SegmentType, index: int) -> None:
         """Writes a VM push command."""
-        pass
+        self.f_vm.write(f"push {segment.name.lower()} {index}")
 
     def write_pop(self, segment: SegmentType, index: int) -> None:
         """Writes a VM pop command."""
         assert segment != SegmentType.CONSTANT, "Cannot pop a constant!"
-        pass
+        self.f_vm.write(f"pop {segment.name.lower()} {index}")
 
     def write_arithmetic(self, command: str) -> None:
         """Writes a VM arithmetic-logical command."""
         assert command in {"add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"}
-        pass
+        self.f_vm.write(command)
 
     def write_label(self, label: str) -> None:
         """Writes a VM label command."""
-        pass
+        self.f_vm.write(f"label {label}")
 
     def write_goto(self, label: str) -> None:
         """Writes a VM goto command."""
-        pass
+        self.f_vm.write(f"goto {label}")
 
     def write_if(self, label: str) -> None:
         """Writes a VM if-goto command."""
-        pass
+        self.f_vm.write(f"if-goto {label}")
 
     def write_call(self, name: str, n_args: int) -> None:
         """Writes a VM call commands."""
-        pass
+        self.f_vm.write(f"call {name} {n_args}")
 
     def write_function(self, name: str, n_vars: int) -> None:
         """Writes a VM function command."""
-        pass
+        self.f_vm.write(f"function {name} {n_vars}")
 
     def write_return(self) -> None:
         """Writes a VM return command."""
-        pass
+        self.f_vm.write("return")
 
     def close(self) -> None:
         """Closes the output file/stream."""
+        print(f"VM file written to [{self.f_vm}]")
         self.f_vm.close()
