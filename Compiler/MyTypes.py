ae8effe1eab729175b156fe0f40a2a71b84f5d31
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-07-17 10:03:43 +0800

    set unknown to -1

--- Compiler/MyTypes.py
+++ Compiler/MyTypes.py
@@ -2,22 +2,33 @@ from enum import Enum, unique
 
 @unique
 class TokenType(Enum):
+    UNKNOWN = -1
     KEYWORD = 0
     SYMBOL = 1
     IDENTIFIER = 2
     INT_CONST = 3
     STRING_CONST = 4
-    UNKNOWN = 5
 
 @unique
 class VarKind(Enum):
+    NONE = -1
     STATIC = 0
     FIELD = 1
     ARG = 2
     VAR = 3
-    NONE = 4
 
 @unique
 class UsageType(Enum):
     USED = 0
     DECLARED = 1
+
+@unique
+class SegmentType(Enum):
+    CONSTANT = 0
+    ARGUMENT = 1
+    LOCAL = 2
+    STATIC = 3
+    THIS = 4
+    THAT = 5
+    POINTER = 6
+    TEMP = 7
