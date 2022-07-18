5d9e7f924bf412752a3d11c5d2c5d79433b518b8
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-07-18 21:11:19 +0800

    added new line char

--- Compiler/VMWriter.py
+++ Compiler/VMWriter.py
@@ -13,49 +13,57 @@ from pathlib import Path
 
 class VMWriter:
 
+    # arithmetic-logical commands
+    al_op2cmd = {
+        '+': "add", '-': "sub", '=': "eq", 
+        '>': "gt", '<': "lt", '&': "and", '|': "or"
+    }
+    al_op2func = {'*': "Math.multiply", '/': "Math.divide"}
+    al_uop2cmd = {'-': "neg", '~': "not"}
+
     def __init__(self, vm_file_path: Path) -> None:
         """Creates a new output .vm file/stream, and prepares it for writing."""
         self.f_vm = open(vm_file_path, "w")
 
     def write_push(self, segment: SegmentType, index: int) -> None:
         """Writes a VM push command."""
-        self.f_vm.write(f"push {segment.name.lower()} {index}")
+        self.f_vm.write(f"push {segment.name.lower()} {index}\n")
 
     def write_pop(self, segment: SegmentType, index: int) -> None:
         """Writes a VM pop command."""
         assert segment != SegmentType.CONSTANT, "Cannot pop a constant!"
-        self.f_vm.write(f"pop {segment.name.lower()} {index}")
+        self.f_vm.write(f"pop {segment.name.lower()} {index}\n")
 
     def write_arithmetic(self, command: str) -> None:
         """Writes a VM arithmetic-logical command."""
         assert command in {"add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"}
-        self.f_vm.write(command)
+        self.f_vm.write(command + '\n')
 
     def write_label(self, label: str) -> None:
         """Writes a VM label command."""
-        self.f_vm.write(f"label {label}")
+        self.f_vm.write(f"label {label}\n")
 
     def write_goto(self, label: str) -> None:
         """Writes a VM goto command."""
-        self.f_vm.write(f"goto {label}")
+        self.f_vm.write(f"goto {label}\n")
 
     def write_if(self, label: str) -> None:
         """Writes a VM if-goto command."""
-        self.f_vm.write(f"if-goto {label}")
+        self.f_vm.write(f"if-goto {label}\n")
 
     def write_call(self, name: str, n_args: int) -> None:
         """Writes a VM call commands."""
-        self.f_vm.write(f"call {name} {n_args}")
+        self.f_vm.write(f"call {name} {n_args}\n")
 
     def write_function(self, name: str, n_vars: int) -> None:
         """Writes a VM function command."""
-        self.f_vm.write(f"function {name} {n_vars}")
+        self.f_vm.write(f"function {name} {n_vars}\n")
 
     def write_return(self) -> None:
         """Writes a VM return command."""
-        self.f_vm.write("return")
+        self.f_vm.write("return\n")
 
     def close(self) -> None:
         """Closes the output file/stream."""
-        print(f"VM file written to [{self.f_vm}]")
+        print(f"VM file written to [{self.f_vm.name}]")
         self.f_vm.close()
